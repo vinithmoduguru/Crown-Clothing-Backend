d2040c05177d1ea75288c2ddd0355f84cb1596b9
vinithmoduguru <vinith1142@gmail.com>
2025-02-13 09:43:11 +0530

    Added bulk and delete routes

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -6,10 +6,19 @@ export const getProducts = async (
   res: Response
 ): Promise<void> => {
   try {
-    const products = await Product.find(
-      { isActive: true },
-      { title: 1, description: 1, price: 1, imageUrl: 1, categoryId: 1 }
-    ).populate("categoryId", "title");
+    // Get category from request query params
+    const categoryId = req.query.categoryId as string;
+    const filter: any = { isActive: true };
+    if (categoryId) {
+      filter.categoryId = categoryId;
+    }
+    const products = await Product.find(filter, {
+      title: 1,
+      description: 1,
+      price: 1,
+      imageUrl: 1,
+      categoryId: 1,
+    }).populate("categoryId", "title");
     // Add Category title to each product
     res.status(200).json(products);
   } catch (error) {
@@ -21,7 +30,8 @@ export const createProduct = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-  const { title, description, price, imageUrl, categoryId }: Partial<IProduct> = req.body;
+  const { title, description, price, imageUrl, categoryId }: Partial<IProduct> =
+    req.body;
   try {
     const newProduct = await Product.create({
       title,
@@ -31,6 +41,39 @@ export const createProduct = async (
       categoryId,
     });
     res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+};
+
+
+export const createBulkProducts = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { products, categoryId } = req.body;
+  const sanitisedProducts = products.map((product: any) => ({
+    title: product.name,
+    imageUrl: product.imageUrl,
+    price: product.price,
+    categoryId: categoryId
+  }))
+  try {
+    const newProducts = await Product.insertMany(sanitisedProducts);
+    res.status(201).json(newProducts);
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+};
+
+export const deleteProduct = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findByIdAndDelete(id);
+    res.status(200).json(product);
   } catch (error) {
     res.status(500).json({ message: (error as Error).message });
   }
